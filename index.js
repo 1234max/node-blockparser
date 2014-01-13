a1009410e31b6b2fa3c41bacd3648755e93703d2
sidazhang <sidazhang89@gmail.com>
2014-01-12 16:11:09 -0800

    function name change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var utils = require( './blockparser/utils' )
 var dirPath = '/Users/alumn/projects/bitcoinmafia/blockchain-read-only/blk00000.dat'
 var blockchain = require( './blockparser/blockchain' )
 
-blockchain.blockchain( dirPath, function( err, fileBuf ) {
+blockchain.openFile( dirPath, function( err, fileBuf ) {
   if ( err ) throw err;
 
   blockchain.readNext( fileBuf )
